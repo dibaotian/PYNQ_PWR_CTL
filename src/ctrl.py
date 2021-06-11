0c65336d97b0bbb83c42e910672ad5873f2e09cb
dibaotian <laba22@163.com>
2021-06-11 15:18:29 +0000

    update

--- src/ctrl.py
+++ src/ctrl.py
@@ -1,14 +1,52 @@
 #!../venv/bin/python3
 # -*- coding: utf-8 -*-
 
+import time
+
 # from pynq import Overlay
 from pynq.overlays.base import BaseOverlay
+#AXI GPIO
 from pynq.lib import AxiGPIO
+# PS GPIO
+from pynq import GPIO
 
-ol = BaseOverlay("base.bit")
 
-led_ip = ol.ip_dict['gpio_leds']
-switches_ip = ol.ip_dict['gpio_switches']
-leds = AxiGPIO(led_ip).channel1
-switches = AxiGPIO(switches_ip).channel1
+start_time = time.time()
+print(f'start to program base overlay: {time.strftime("%I:%M:%S")}')
+base = BaseOverlay("base.bit")
+print(f'program base overlay complete: {time.strftime("%I:%M:%S")}')
+end_time = time.time()
+prog_time = end_time - start_time
+print(f'program the overlay took: {prog_time} seconds')
+
+# 通过AXIGPIO 操作
+# btns = base.btns_gpio
+# leds = base.leds_gpio
+
+# print(btns.register_map)
+
+# print(leds.register_map)
+
+# leds.register_map.GPIO_DATA.Channel_1_GPIO_DATA = 8
+# leds.register_map.GPIO_DATA[2:0] = 5
+
+# pynq.gpio module is a driver for reading and writing PS GPIO pins on a board. PS GPIO pins are not connected to the PL
+# gpio = GPIO.get_gpio_base()
+# print(gpio)
+
+rgbleds = [base.rgbleds[i] for i in range(4, 6)]
+leds = [base.leds[i] for i in range(4)]
+
+# Toggle board LEDs leaving small LEDs lit
+for i in range(8):
+    [l.off() for l in leds]
+    [rgbled.off() for rgbled in rgbleds]
+    time.sleep(.2)
+    [l.on() for l in leds]
+    [rgbled.on(1) for rgbled in rgbleds]
+    time.sleep(.2)
+
+[rgbled.off() for rgbled in rgbleds]
+
+
 
--- src/views.py
+++ src/views.py
@@ -1,12 +1,50 @@
 #!../venv/bin/python3
 # -*- coding: utf-8 -*-
+
+import time
+
+
 from flask import Flask, render_template
+
+# from pynq import Overlay
+from pynq.overlays.base import BaseOverlay
+#AXI GPIO
+from pynq.lib import AxiGPIO
+# PS GPIO
+from pynq import GPIO
+
 app = Flask(__name__)
+
+base = BaseOverlay("base.bit")
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/switch_clicked')
+def switch_clicked():
+
+    responses = {'code': 1, 'msg': 'failure', 'data': {}}
+
+    rgbleds = [base.rgbleds[i] for i in range(4, 6)]
+    leds = [base.leds[i] for i in range(4)]
+
+    # Toggle board LEDs leaving small LEDs lit
+    for i in range(8):
+        [l.off() for l in leds]
+        [rgbled.off() for rgbled in rgbleds]
+        time.sleep(.2)
+        [l.on() for l in leds]
+        [rgbled.on(1) for rgbled in rgbleds]
+        time.sleep(.2)
+
+    [rgbled.off() for rgbled in rgbleds]
+
+    responses['code'] = 0
+    responses['msg'] = 'success'
+
+    return responses
       
 if __name__ == '__main__':
     app.debug = True
